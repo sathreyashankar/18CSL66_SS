89c8c85dbd64498c8697e74ca05c3a853ed29bad
Udayashankar N K <athreya036@gmail.com>
2023-07-06 04:28:08 +0530

    Shift-Reduce Parsing of grammar - v1

--- labProgram4/lp4.c
+++ labProgram4/lp4.c
@@ -4,25 +4,35 @@
 
 int k = 0, z = 0, i = 0, j = 0, c = 0;
 char a[16], ac[20], stk[15], act[10];
+
 void check();
+
 void main()
 {
     printf("GRAMMAR is E->E+T|T \n T->T*F|F \n F->(E) \n F->ID");
+   
     printf("Enter input string");
-    // gets(a);
     scanf("%s", a);
     c = strlen(a);
+
     strcpy(act, "SHIFT->");
+
     printf("stack \t input \t action");
+    
+    // k --> normal iteration, i-->top of stack, j->
+
     for (k = 0, i = 0; j < c; k++, i++, j++)
     {
         if (a[j] == 'i' && a[j + 1] == 'd')
         {
+            // Shift id from input a to Stack stk
             stk[i] = a[j];
             stk[i + 1] = a[j + 1];
             stk[i + 2] = '\0';
+            // And remove id from input
             a[j] = ' ';
             a[j + 1] = ' ';
+
             printf("\n$%s\t%s$\t%sid", stk, a, act);
             check();
         }
@@ -33,14 +43,16 @@ void main()
             a[j] = ' ';
             printf("\n$%s\t%s$\t%ssymbols", stk, a, act);
             check();
-        } // end else
-    }     // end for
-} // end main
-//--------------------------------------------------------------------------------------------
+        } 
+    }     
+} 
+
+
 void check()
 {
     strcpy(ac, "REDUCE");
-    // first for
+    
+    // Check with F->(E)
     for (z = 0; z < c; z++)
         if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
         {
@@ -50,7 +62,7 @@ void check()
             printf("\n$%s\t%s$\t%s", stk, a, ac);
             i = i - 2;
         }
-    // 2nd for
+    // Check with F->id
     for (z = 0; z < c; z++)
         if (stk[z] == 'i' && stk[z + 1] == 'd')
         {
@@ -59,7 +71,7 @@ void check()
             printf("\n$%s\t%s$\t%s", stk, a, ac);
             j++;
         }
-    // 3rd for
+    // Check with T->T*F
     for (z = 0; z < c; z++)
     {
         if (stk[z] == 'T' && stk[z + 1] == '*' && stk[z + 2] == 'F')
@@ -70,17 +82,20 @@ void check()
             printf("\n$%s\t%s$\t%s", stk, a, ac);
             i = i - 2;
         }
+        // Check with T->F
         else if (stk[z] == 'F')
         {
             stk[z] = 'T';
             printf("\n$%s\t%s$\t%s", stk, a, ac);
         }
     }
-    // 4th for
+    // Check with E->E+T
     for (z = 0; z < c; z++)
     {
+        // Don't do anything for E->E+T*
         if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'T' && stk[z + 3] == '*')
             break;
+        // Check with E->E+T
         if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'T')
         {
             if (a[j + 1] == '*')
@@ -92,12 +107,11 @@ void check()
                 stk[z + 2] = '\0';
                 printf("\n$%s\t%s$\t%s", stk, a, ac);
                 i = i - 2;
-            } // end else
-        }     // end if
+            } 
+        }     
         else if (stk[z] == 'T')
         {
             stk[z] = 'E';
             printf("\n$%s\t%s$\t%s", stk, a, ac);
         }
-    } // for ends
-} // check()
+    } 
